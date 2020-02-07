8c025daca3e5ca8ea5d5a82212b85292bc672d11
chip <scarpenter@btinternet.com>
2020-02-07 16:13:46 +0000

    Start adding click

--- user_snapshot/snapshot.py
+++ user_snapshot/snapshot.py
@@ -1,7 +1,15 @@
 import boto3
+import click
 
-if __name__ == '__main__':
-    session = boto3.Session(profile_name='snapshot')
-    ec2 = session.resource('ec2')
+
+session = boto3.Session(profile_name='snapshot')
+ec2 = session.resource('ec2')
+
+@click.command()
+def list_instances():
+    "List eC2 instances"
     for i in ec2.instances.all():
         print(i)
+
+if __name__ == '__main__':
+    list_instances()
